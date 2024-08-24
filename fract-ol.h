e09b3daccb09bf202a63ac5443b87ec38336f9b5
Nick Boer <nboer@c1b2c1.42berlin.de>
2024-08-24 20:45:39 +0200

    new

--- fract-ol.h
+++ fract-ol.h
@@ -6,7 +6,7 @@
 /*   By: nboer <nboer@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/18 20:26:15 by nboer             #+#    #+#             */
-/*   Updated: 2024/08/24 19:49:25 by nboer            ###   ########.fr       */
+/*   Updated: 2024/08/24 20:45:19 by nboer            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,11 +23,14 @@ typedef struct	s_complex{
 
 typedef struct s_fractol
 {
-	t_complex	complex;
+	t_complex	num;
 	void	*mlx_win_ptr; // mlx_new_window
 	void	*mlx_ptr;  //mlx_init
 	void	*pix_ptr; // pointer to pixels
 	void	*mlx_img_ptr; //pointer to image
+	int		colour;
+	double	max_iterations;
+	
 
 }	t_fractol;
 
