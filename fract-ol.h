865185c3013a54a3c3c1e05453db69794e4adc10
boernick <nick.boer@hotmail.com>
2024-08-25 22:11:32 +0200

    update

--- fract-ol.h
+++ fract-ol.h
@@ -3,18 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   fract-ol.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nboer <nboer@student.42.fr>                +#+  +:+       +#+        */
+/*   By: nick <nick@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/18 20:26:15 by nboer             #+#    #+#             */
-/*   Updated: 2024/08/24 20:45:19 by nboer            ###   ########.fr       */
+/*   Updated: 2024/08/25 16:57:30 by nick             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+// DESKTOP: /home/nick/42Berlin/fract-ol/includes/
+// PC 42: 	/home/nboer/git/fract-ol/includes/
+
 #include <unistd.h>
 #include <stdlib.h>
-#include "/home/nboer/git/fract-ol/includes/minilibx-linux/mlx.h"
-#include "/home/nboer/git/fract-ol/includes/ft_printf/ft_printf.h"
-#include "/home/nboer/git/fract-ol/includes/Libft/libft.h"
+#include "/home/nick/42Berlin/fract-ol/includes/minilibx-linux/mlx.h"
+#include "/home/nick/42Berlin/fract-ol/includes/ft_printf/ft_printf.h"
+#include "/home/nick/42Berlin/fract-ol/includes/Libft/libft.h"
 
 typedef struct	s_complex{
 	double	y; //complex
@@ -24,10 +27,10 @@ typedef struct	s_complex{
 typedef struct s_fractol
 {
 	t_complex	num;
-	void	*mlx_win_ptr; // mlx_new_window
+	void	*win_ptr; // mlx_new_window
 	void	*mlx_ptr;  //mlx_init
 	void	*pix_ptr; // pointer to pixels
-	void	*mlx_img_ptr; //pointer to image
+	void	*img_ptr; //pointer to image
 	int		colour;
 	double	max_iterations;
 	
@@ -35,4 +38,5 @@ typedef struct s_fractol
 }	t_fractol;
 
 void	ft_error(void);
+double	scale(double num, double n_min, double n_max, double o_min, double o_max);
 
--- fractol.c
+++ fractol.c
@@ -3,68 +3,111 @@
 /*                                                        :::      ::::::::   */
 /*   fractol.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nboer <nboer@student.42.fr>                +#+  +:+       +#+        */
+/*   By: nick <nick@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/18 20:50:15 by nboer             #+#    #+#             */
-/*   Updated: 2024/08/24 20:45:16 by nboer            ###   ########.fr       */
+/*   Updated: 2024/08/25 22:11:20 by nick             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fract-ol.h"
 
-// Setup MiniLibX.
-// Create a window, image in MiniLibX.
-// Iterate through every pixel of the window.
 // For every pixel, calculate the complex number it represents and put it into the suite.
 // If the suite diverges, color the pixel in white.
 // If the suite converges, color the pixel in black.
-#define RES_Y 1200
-#define RES_X 800
+#define RES_Y 800
+#define RES_X 1200
 #define MAX_ITERATION 10
 
-void	fractol_calc(t_fractol *frac)
+// The mandelbrot's visual exists between the values 2 and -2, therefore values
+// in a screen of size RES_Y x RES_X need to be remapped accordingly. Function 
+// takes a value in a set domain between min, max, and returns remapped value.
+double	remap(double num, double n_min, double n_max, double o_min, double o_max)
 {
-	ft_printf("calc fractol");
+	return (n_max - n_min) * (num - o_min) / (o_max - o_min) + n_min;
+}
 
-	int	pixel
+// if you square the complex number you get z = (z^2 - y^2) + 2xyi
+t_complex	square_com(t_complex z)
+{
+	t_complex	res;
+	
+	res.x = (z.x * z.x) - (z.y - z.y);
+	res.y = 2 * z.x * z.y;
+	return (res);
+}
+
+t_complex	sum_com(t_complex *z1, t_complex *z2)
+{
+	t_complex	res;
+
+	res.x = z1->x + z2->x;
+	res.y = z1->y + z2->y;
+	return (res);	
+}
+
+double	hypotenuse(double a, double b)
+{
+	return (square_root(sum(square(a), square(b))));
+}
+void	fractol_calc_pix(int x, int y, t_fractol *frac)
+{
+	ft_printf("calculating mandelbrot...");
+	t_complex	z;
+	t_complex	w;
+
+	//first iteration
+	z.x = 0;
+	z.y = 0;
+
+	c.x = remap(x, -2, 2, 0, RES_X)
+	c.y = remap(x, 2, -2, 0, RES_Y);
+	// voor elke pixel moet ik checken of hij binnen of buiten de hypotenuse valt.
+	// wat is hij? de oplossing van de mandelbrot formule voor een specifiek punt in de grafiek waarin (x,y) = (real number, complex number)
+	// totdat de waarde van hypotenuse groter dan 2 wordt, ga kan als "binnen" de fractol gekleurd worden 
+	frac = sum_com(square_com(z), c);
+
+	while (hypotenuse(frac->num.y, frac->num.x) < 2)
+		//	roep de calculation aan
 
 	while (frac->num.y < RES_Y)
 	{
 		frac->num.y++;
-		while(frac->num.x < RES_X)
+		while (frac->num.x < RES_X)
 		{
-			frac->num.y * frac->num.y +  
 			//calculate mandelbrot;
 			//calculate colour based on iterations;
-			z.x++;
+			frac->num.x++;
 		}
 	}
 
-
 }
-
-void	fractol_init(int res_y, int res_x)
+// function takes XY size of screen as input and 
+void	fractol_init(int res_y, int res_x, t_fractol *frac)
 {
-	void	*mlx;
-	void	*win;
-	void	*img;
+	int	pixel_bits;
+	int	endian;
+	int line_bytes;
 
-	mlx = mlx_init();
-	win = mlx_new_window(mlx, res_y, res_x, "mandelbrot");
-	if (win == NULL)
+	frac->mlx_ptr = mlx_init();
+	frac->win_ptr = mlx_new_window(frac->mlx_ptr, res_y, res_x, "mandelbrot");
+	if (frac->win_ptr == NULL)
 		ft_error();
-	img = mlx_new_image(mlx, RES_Y, RES_X);
-	mlx_pixel_put(mlx, win, RES_X / 2, RES_Y / 2, 0xFFFFFF);
+	frac->img_ptr = mlx_new_image(frac->mlx_ptr, RES_Y, RES_X);
+	
 	// eendimensionale array met alle coordinaten met mlx functie aanroepen
+	frac->buff = mlx_get_data_addr(frac->img_ptr, &pixel_bits, &line_bytes, &endian);
+
+	
 	// per pixel in de mandelbrot functie aanroepen en dan kleur toewijzen op basis van het aantal iteraties
-	mlx_pixel_put(mlx, win, RES_X / 2 + 2, RES_Y / 2, 0xFFFFFF); // this is really slow so i need to use mlx_image_to_window()
-	mlx_pixel_put(mlx, win, RES_X / 2 + 1, RES_Y / 2, 0xF000FF);
 	fractol_calc();
 	// mlx_put_image_to_window();
 }
 
 int	main(int argc, char **argv)
 {
+	t_fractol	frac;
+
 	if (argc == 1)
 		ft_printf("Please enter command as: \n	- mendelbrot \n	- julia \n");
 	if (argc == 2)
@@ -72,7 +115,7 @@ int	main(int argc, char **argv)
 		if (ft_strncmp(argv[1], "mandelbrot", 10))
 		{
 			ft_printf("works");
-			fractol_init(RES_Y, RES_X);
+			fractol_init(RES_Y, RES_X, &frac);
 		}
 	}
 	return (0);
